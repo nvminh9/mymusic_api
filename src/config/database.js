d316bddaf1919e04b76e8f60590823915d06a920
minhngo <minhcorn.2@gmail.com>
2025-11-21 08:43:49 +0700

    [Update] update database config (change connect to database method)

--- src/config/database.js
+++ src/config/database.js
@@ -4,14 +4,15 @@ dotenv.config();
 
 // Connect to database
 // Cách 1
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, String(process.env.DB_PASSWORD), {
-  host: process.env.DB_HOST || "localhost",
-  dialect: "postgres",
-  port: process.env.DB_PORT || 5432,
-  logging: false, // Tắt log SQL
-});
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, String(process.env.DB_PASSWORD), {
+//   host: process.env.DB_HOST || "localhost",
+//   dialect: "postgres",
+//   port: process.env.DB_PORT || 5432,
+//   logging: false, // Tắt log SQL
+// });
 // Cách 2
 // const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`) //
+const sequelize = new Sequelize(`postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`) //
 
 // Test connection
 async () => {
